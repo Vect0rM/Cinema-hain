8dfcb3766727ddb9fbab70085f7a0c515080e9bf
Misha <m.skvortsovvv@gmail.com>
2022-05-06 21:31:52 +0300

    Initial commit

---
+++ src/test/java/com/example/cinemahain/CinemaHainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinemahain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaHainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
